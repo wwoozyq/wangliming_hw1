a58ab9015d71d977604628ec8731f3ad2592c24e
wangliming <2744679634@qq.com>
2024-07-02 17:06:31 +0800

    初始提交

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: F:/zzy/code/algebra
+# Install script for directory: /Users/wlm/projects/wangliming_hw1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files (x86)/Dev-Cpp/MinGW64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/zzy/code/algebra/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/wlm/projects/wangliming_hw1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/algebra.c
+++ src/algebra.c
@@ -10,58 +10,188 @@ Matrix create_matrix(int row, int col)
     return m;
 }
 
-Matrix add_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+Matrix add_matrix(Matrix a, Matrix b) {
+    if (a.rows != b.rows || a.cols != b.cols) {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0); 
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] + b.data[i][j];
+        }
+    }
+    return result;
+}
+Matrix sub_matrix(Matrix a, Matrix b) {
+    if (a.rows != b.rows || a.cols != b.cols) {
+        printf("Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] - b.data[i][j];
+        }
+    }
+    return result;
+}
+Matrix mul_matrix(Matrix a, Matrix b) {
+    if (a.cols != b.rows) {
+        printf("Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, b.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < b.cols; j++) {
+            result.data[i][j] = 0;
+            for (int k = 0; k < a.cols; k++) {
+                result.data[i][j] += a.data[i][k] * b.data[k][j];
+            }
+        }
+    }
+    return result;
+}
+Matrix scale_matrix(Matrix a, double k) {
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] * k;
+        }
+    }
+    return result;
 }
 
-Matrix sub_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+Matrix transpose_matrix(Matrix a) {
+    Matrix result = create_matrix(a.cols, a.rows);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[j][i] = a.data[i][j];
+        }
+    }
+    return result;
 }
 
-Matrix mul_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+double det_matrix(Matrix a) {
+    if (a.rows != a.cols) {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0;
+    }
+
+    if (a.rows == 1)return a.data[0][0];
+    
+    double result = 0.0;
+
+    for (int i = 0; i < a.rows; i++) {
+        Matrix submatrix = create_matrix(a.rows - 1, a.rows - 1);
+        for (int row = 1; row < a.rows; row++) {
+            int colCount = 0;
+            for (int col = 0; col < a.rows; col++) {
+                if (col == i) {
+                    continue;
+                }
+                submatrix.data[row - 1][colCount] = a.data[row][col];
+                colCount++;
+            }
+        }
+        double sub_det = det_matrix(submatrix);
+        result += (i % 2 == 0 ? 1 : -1) * a.data[0][i] * sub_det;
+    }
+    return result;
 }
 
-Matrix scale_matrix(Matrix a, double k)
-{
-    // ToDo
-    return create_matrix(0, 0);
+Matrix inv_matrix(Matrix a) {
+    if (a.rows != a.cols) {
+        printf("Error: The matrix must be a square matrix.\n");
+        return create_matrix(0, 0);
+    }
+    double det = det_matrix(a);
+    if (det == 0) {
+        printf("Error: The matrix is singular.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix adj = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            Matrix sub = create_matrix(a.rows - 1, a.cols - 1);
+            int sub_i = 0, sub_j = 0;
+            for (int row = 0; row < a.rows; row++) {
+                if (row == i) continue;
+                sub_j = 0;
+                for (int col = 0; col < a.cols; col++) {
+                    if (col == j) continue;
+                    sub.data[sub_i][sub_j] = a.data[row][col];
+                    sub_j++;
+                }
+                sub_i++;
+            }
+            double sub_det = det_matrix(sub);
+            adj.data[j][i] = ((i + j) % 2 == 0 ? 1 : -1) * sub_det;
+            if (adj.data[j][i] == -0) adj.data[j][i] = 0;
+        }
+    }
+    Matrix inv = scale_matrix(adj, 1.0 / det);
+    return inv;
 }
 
-Matrix transpose_matrix(Matrix a)
-{
-    // ToDo
-    return create_matrix(0, 0);
+int rank_matrix(Matrix a) {
+    int m = a.rows;
+    int n = a.cols;
+    int rank = (m < n) ? m : n;
+
+    for (int row = 0; row < rank; row++) {
+        // 如果当前主对角线元素 a[row][row] 为零，查找下面的非零元素并交换
+        if (a.data[row][row] == 0) {
+            int reduce = 1;
+            for (int k = row + 1; k < m; k++) {
+                if (a.data[k][row] != 0) {
+                    // 交换行
+                    for (int j = 0; j < n; j++) {
+                        double temp = a.data[row][j];
+                        a.data[row][j] = a.data[k][j];
+                        a.data[k][j] = temp;
+                    }
+                    reduce = 0;
+                    break;
+                }
+            }
+            // 如果找不到非零元素，秩减一，并跳过该列
+            if (reduce) break;}
+        // 进行高斯消元，消去当前列下面的所有元素
+        for (int r = row + 1; r < m; r++) {
+            if (a.data[r][row] != 0) {
+                double mult = a.data[r][row] / a.data[row][row];
+                for (int col = 0; col < n; col++) {
+                    a.data[r][col] -= mult * a.data[row][col];
+                }
+            }
+        }
+    }
+
+        rank = m;
+    for (int i = m - 1; i >= 0; i--) {
+        int index = 0;
+        for (int j = 0; j < n; j++){
+            if(fabs(a.data[i][j])>0.000000001){
+                index = 1;
+                break;
+            }
+        }
+        if (index == 0) rank--;
+    }
+    return rank;
 }
 
-double det_matrix(Matrix a)
-{
-    // ToDo
-    return 0;
-}
-
-Matrix inv_matrix(Matrix a)
-{
-    // ToDo
-    return create_matrix(0, 0);
-}
-
-int rank_matrix(Matrix a)
-{
-    // ToDo
-    return 0;
-}
-
-double trace_matrix(Matrix a)
-{
-    // ToDo
-    return 0;
+double trace_matrix(Matrix a) {
+    if (a.rows != a.cols) {
+        printf("Error: The matrix must be a square matrix.\n");
+        return 0;
+    }
+    double trace = 0;
+    for (int i = 0; i < a.rows; i++) {
+        trace += a.data[i][i];
+    }
+    return trace;
 }
 
 void print_matrix(Matrix a)
